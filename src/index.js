119e8820352cd138d5b8c0d1ee781c102e494500
srabalogh <linecrist@hotmail.com>
2018-04-11 21:36:35 -0300

    add code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDVbp8S55paujARuAx9hxJH8z3pROqr6kg';
 
@@ -21,9 +22,9 @@ class App extends Component{
     });
   }
   render(){
-    console.log('ha')
     return <div>
       <SearchBar  />
+      <VideoDetail video = {this.state.videos[0]}/>
       <VideoList videos = {this.state.videos} />
     </div>
   }
