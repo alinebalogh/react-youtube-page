57f7c1a9c8728e2afb163f8d22f5b33498ca7cb0
Aline Balogh <aline.balogh@wunderman.com>
2018-04-16 10:12:04 -0300

    add style

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,12 +8,17 @@ class SearchBar extends Component{
 
 	}
 	render(){
+		const useTag = '<use class="searchbar__icon" xlink:href="././assets/sprite/sprite.svg#icon-search" />';
 		return (
 			<div className="searchbar">
-				<input 
-				className="searchbar__input"
-				value={this.state.term}
-				onChange= {(event) => this.onInputChange(event.target.value)} />
+				<div className="searchbar__wrap">
+					<input 
+					placeholder="Search"
+					className="searchbar__input"
+					value={this.state.term}
+					onChange= {(event) => this.onInputChange(event.target.value)} />
+					<span><svg dangerouslySetInnerHTML={{__html: useTag }} /></span>
+				</div>
 			</div>
 		)
 	}
